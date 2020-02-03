ce0a40a4cf80960acb9905b0bc50584bc3f97291
ganciaux <ghislain.anciaux@live.fr>
2020-02-03 16:42:29 +0100

    Init, set views and models application store

---
+++ disquaire_project/store/views.py
@@ -0,0 +1,47 @@
+#from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Album, Artist, Contact, Booking
+
+def index(request):
+    # request albums
+    albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
+    # then format the request.
+    # note that we don't use album['name'] anymore but album.name
+    # because it's now an attribute.
+    formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
+    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
+    return HttpResponse(message)
+
+def listing(request):
+    albums = Album.objects.filter(available=True)
+    formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
+    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
+    return HttpResponse(message)
+
+def detail(request, album_id):
+    album = Album.objects.get(pk=album_id)
+    artists = " ".join([artist.name for artist in album.artists.all()])
+    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album.title, artists)
+    return HttpResponse(message)
+
+def search(request):
+    query = request.GET.get('query')
+    if not query:
+        albums = Album.objects.all()
+    else:
+        # title contains the query and query is not sensitive to case.
+        albums = Album.objects.filter(title__icontains=query)
+
+    if not albums.exists():
+        albums = Album.objects.filter(artists__name__icontains=query)
+
+    if not albums.exists():
+        message = "Misère de misère, nous n'avons trouvé aucun résultat !"
+    else:
+        albums = ["<li>{}</li>".format(album.title) for album in albums]
+        message = """
+            Nous avons trouvé les albums correspondant à votre requête ! Les voici :
+            <ul>{}</ul>
+        """.format("</li><li>".join(albums))
+
+    return HttpResponse(message)
