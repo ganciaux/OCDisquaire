2627fd539b6a7a4f5362c53a6803427cbc9c8c0e
ganciaux <ghislain.anciaux@live.fr>
2020-02-04 11:13:38 +0100

    Add page 404/500 and search form

--- disquaire_project/disquaire_project/settings.py
+++ disquaire_project/disquaire_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9(zjwhm%!-pyy6kj+x563mvdfvz$zlb52+h!=*k7z77*zf$8@0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
